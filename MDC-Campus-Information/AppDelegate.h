26bfe51832d405389ebd4f3df1aeeea1cb5c57d6
alive <matthewschwartz@mbp>
2017-10-08 18:46:19 -0400

    Initial Commit

---
+++ MDC-Campus-Information/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDC-Campus-Information
+//
+//  Created by alive on 10/8/17.
+//  Copyright © 2017 Matthew Schwartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
